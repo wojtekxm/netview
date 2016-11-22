cea0f867dee3fb4b9f979a317bd98283c7ade40f
Wojciech Marciniak <wojtekxm@gmail.com>
2016-11-23 00:40:23 +0100

    sprawdzam projekty mavena (Windows, NetBeans)

---
+++ mavenproject123/src/main/java/com/mycompany/mavenproject123/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.mycompany.mavenproject123;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println( "Hello from the other side" );
+    }
+}

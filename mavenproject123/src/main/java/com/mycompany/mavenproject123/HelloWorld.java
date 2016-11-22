35a7724c56fbe646ffc247e6db49321586664e01
Wojciech Marciniak <wojtekxm@gmail.com>
2016-11-23 00:47:50 +0100

    sprawdzam maven i NetBeansa

--- mavenproject123/src/main/java/com/mycompany/mavenproject123/HelloWorld.java
+++ mavenproject123/src/main/java/com/mycompany/mavenproject123/HelloWorld.java
@@ -3,5 +3,6 @@ package com.mycompany.mavenproject123;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println( "Hello from the other side" );
+        System.out.println( "test maven-netbeans-git" );
     }
 }

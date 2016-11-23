c6cf56cd203197639ab476537c69d19367ccccea
Wojciech Marciniak <wojtekxm@gmail.com>
2016-11-23 01:23:15 +0100

    tylko Main.java

--- HelloMaven/src/main/java/team03/hellomaven/Main.java
+++ HelloMaven/src/main/java/team03/hellomaven/Main.java
@@ -2,6 +2,6 @@ package team03.hellomaven;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Hello Maven" );
+        System.out.println( "Make git great, again" );
     }
 }

f6306844fdcf49728933e307128e7e31d3828b79
Wojciech Marciniak <wojtekxm@gmail.com>
2016-12-05 00:52:16 +0100

    SurveyItem, examineController jeszcze nie gotowy

--- local/NetworkMonitor/src/main/java/zesp03/Management.java
+++ local/NetworkMonitor/src/main/java/zesp03/Management.java
@@ -1,9 +1,14 @@
 package zesp03;
 
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.Instant;
-import java.util.Collection;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -18,10 +23,8 @@ public class Management {
     }
 
     public static boolean isValidControllerName(String name) {
-        if(name == null)return false;
-        if(name.length() < 1)return false;
-        if(name.length() > CONTROLLER_NAME_MAX_CHARS)return false;
-        return Unicode.noSurrogates(name);
+        if (name.length() < 1) return false;
+        return name.length() <= CONTROLLER_NAME_MAX_CHARS && Unicode.noSurrogates(name);
     }
 
     public static boolean isCompatibleDeviceName(String name) {
@@ -68,6 +71,7 @@ public class Management {
     }
 
     public void examineAll() throws SQLException, SNMPException {
+        final HashMap<Integer, String> map = new HashMap<>();
         String sql = "SELECT id, ipv4 FROM controller";
         try(Connection con = Database.connect();
             Statement st = con.createStatement();
@@ -75,82 +79,147 @@ public class Management {
             while( res.next() ) {
                 int id = res.getInt("id");
                 String ipv4 = res.getString("ipv4");
-                examineController(id, ipv4, con);
+                map.put(id, ipv4);
             }
         }
+        map.forEach( (controllerId, ipv4) -> {
+            // czy ten controllerId nadal będzie istnieć w bazie po zakończeniu powyższego Connection [?]
+            try {
+                examineController(controllerId, ipv4);
+            }
+            catch(SQLException exc) {
+                log( "examineAll", exc );
+            }
+        } );
     }
 
-    private void examineController(int controllerId, String ipv4, Connection con) throws SQLException, SNMPException {
-        final List<DeviceInfo> list = snmp.queryDevices(ipv4);
-        makeCompatibleNames(list);
-        makeUniqueNames(list);
+    protected void examineController(int controllerId, String ipv4) throws SQLException {
+        HashMap<String, SurveyItem> surveyed;
+        try {
+            surveyed = filterDevices( snmp.queryDevices(ipv4) );
+        }
+        catch(SNMPException exc) {
+            log( "examineController", exc );
+            return;
+        }
+        // przechowuje mapę urządzeń odczytaną z bazy danych
+        // mapuje nazwę urządzenia do jego ID
+        final HashMap<String, Integer> found = new HashMap<>();
+
         Instant now = Instant.now();
         long longTS = now.getEpochSecond();
         if(longTS <= 0 || longTS > Integer.MAX_VALUE)
             throw new IllegalStateException("timestamp overflow");
         int timestamp = (int)longTS;
-        for(DeviceInfo info : list) {
-            String sql1 = "SELECT id FROM device WHERE name = ?";
-            int deviceId = -1;
-            try( PreparedStatement prep = con.prepareStatement(sql1) ) {
-                prep.setString(1, info.getName() );
-                try( ResultSet res = prep.executeQuery() ) {
-                    if( res.next() ) {
-                        deviceId = res.getInt("id");
+
+        /*try( Connection con = Database.connect() ) {
+            for(DeviceInfo info : list) {
+                String sql1 = "SELECT id FROM device WHERE name = ?";
+                int deviceId = -1;
+                try( PreparedStatement prep = con.prepareStatement(sql1) ) {
+                    prep.setString(1, info.getName() );
+                    try( ResultSet res = prep.executeQuery() ) {
+                        if( res.next() ) {
+                            deviceId = res.getInt("id");
+                        }
                     }
                 }
-            }
-            if(deviceId == -1) {
-                String sql2 = "INSERT INTO device (name, is_known, description, controller_id) VALUES (?, FALSE, NULL, ?)";
-                try( PreparedStatement prep = con.prepareStatement(sql2, PreparedStatement.RETURN_GENERATED_KEYS) ) {
-                    prep.setString( 1, info.getName() );
-                    prep.setInt( 2, controllerId );
+                if(deviceId == -1) {
+                    String sql2 = "INSERT INTO device (name, is_known, description, controller_id) VALUES (?, FALSE, NULL, ?)";
+                    try( PreparedStatement prep = con.prepareStatement(sql2, PreparedStatement.RETURN_GENERATED_KEYS) ) {
+                        prep.setString( 1, info.getName() );
+                        prep.setInt( 2, controllerId );
+                        prep.executeUpdate();
+                        try( ResultSet res = prep.getGeneratedKeys() ) {
+                            res.next();
+                            deviceId = res.getInt(1);
+                        }
+                    }
+                }
+                String sql3 = "INSERT INTO device_survey (`timestamp`, is_enabled, clients_sum, device_id) VALUES (?, ?, ?, ?)";
+                try(PreparedStatement prep = con.prepareStatement(sql3)) {
+                    prep.setInt(1, timestamp);
+                    prep.setBoolean(2, info.isEnabled());
+                    prep.setInt(3, info.getClients());
+                    prep.setInt(4, deviceId);
                     prep.executeUpdate();
-                    try( ResultSet res = prep.getGeneratedKeys() ) {
-                        res.next();
-                        deviceId = res.getInt(1);
-                    }
                 }
             }
-            String sql3 = "INSERT INTO device_survey (`timestamp`, is_enabled, clients_sum, device_id) VALUES (?, ?, ?, ?)";
-            try(PreparedStatement prep = con.prepareStatement(sql3)) {
-                prep.setInt(1, timestamp);
-                prep.setBoolean(2, info.isEnabled());
-                prep.setInt(3, info.getClients());
-                prep.setInt(4, deviceId);
-                prep.executeUpdate();
-            }
+        }*/
+    }
+
+    protected static class SurveyItem {
+        private int id;
+        private final int clients;
+        private final boolean enabled;
+
+        SurveyItem(int clients, boolean isEnabled) {
+            this.id = -1;
+            this.clients = clients;
+            this.enabled = isEnabled;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getClients() {
+            return clients;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setId(int id) {
+            this.id = id;
         }
     }
 
-    protected void makeCompatibleNames(List<DeviceInfo> list) {
+    /**
+     * Transformuje listę urządzeń w mapę gdzie kluczem jest nazwa urządzenia.
+     * Nazwy urządzeń w mapie będą przekształcone do kompatybilnej postaci za pomocą getCompatibleDeviceName.
+     * W przypadku wielokrotnego wystąpienia nazwy w liscie, w mapie zostanie wpisany pierwszy element listy z daną nazwą.
+     * Id każdego urządzenia w mapie będzie mieć wartość domyślną czyli -1.
+     * @param list lista informacji o urządzeniach zwrócona przez SNMPHandler.
+     *             Lista nie będzie modyfikowana przez tą metodę.
+     * @return mapa utworzona na podstawie przefiltrowanej listy urządzeń
+     */
+    protected HashMap<String, SurveyItem> filterDevices(List<DeviceInfo> list) {
+        final HashMap<String, SurveyItem> result = new HashMap<>();
         for(DeviceInfo info : list) {
-            if( ! isCompatibleDeviceName( info.getName() ) ) {
-                info.setName(getCompatibleDeviceName(info.getName()));
-                log("makeCompatibleNames", "Name \"" + info.getName() + "\" is not compatible.");
+            String name = info.getName();
+            if( ! isCompatibleDeviceName(name) ) {
+                String old = name;
+                name = getCompatibleDeviceName(name);
+                log("filterDevices", "Device name \"" + old + "\" is not compatible.",
+                        "Converted to \"" + name + "\".");
+            }
+            if( result.containsKey(name) ) {
+                log("filterDevices", "Device name \"" + name + "\" occurs more than once.",
+                        info.toString() + " will be rejected.");
+            }
+            else {
+                SurveyItem s = new SurveyItem( info.getClients(), info.isEnabled() );
+                result.put(name, s);
             }
         }
+        return result;
     }
 
-    protected void makeUniqueNames(List<DeviceInfo> list) {
-        final HashMap<String, DeviceInfo> map = new HashMap<>();
-        Iterator<DeviceInfo> iter = list.iterator();
-        while( iter.hasNext() ) {
-            DeviceInfo info = iter.next();
-            DeviceInfo original = map.get( info.getName() );
-            if(original != null) {
-                iter.remove();
-                log("makeUniqueNames", "Name \"" + info.getName() + "\" occurs more than once. " +
-                        original.toString() + " kept " + info.toString() + " rejected.");
-            }
-            else map.put(info.getName(), info);
-        }
-    }
-
-    private void log(String method, String message) {
+    protected void log(String method, String message, String... extra) {
         // !!! zamień na coś lepszego
-        System.out.println("Management log [" + method + "]:");
+        DateTimeFormatter form = DateTimeFormatter.ofPattern("uuuu LLL dd, HH:mm:ss");
+        String dt = LocalDateTime.now().format(form);
+        System.out.println(dt + " Management log (invoked by " + method + "):");
         System.out.println(message);
+        for(String e : extra) {
+            System.out.println(e);
+        }
         System.out.println();
+    }
+
+    protected void log(String method, Throwable exception, String... extra) {
+        log(method, exception.toString(), extra);
     }
 }
--- local/NetworkMonitor/src/test/java/zesp03/TestManagement.java
+++ local/NetworkMonitor/src/test/java/zesp03/TestManagement.java
@@ -1,7 +1,14 @@
 package zesp03;
 
 import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class TestManagement {
     @Test
@@ -62,5 +69,55 @@ public class TestManagement {
         String original = "a" + TestUnicode.BAT_STRING + "b";
         String expected = "a??b";
         assertEquals( Management.getCompatibleDeviceName(original), expected );
+    }
+
+    @Test
+    public void filterDevices_empty_empty() throws IOException {
+        Management mgm = new Management();
+        assertTrue( mgm.filterDevices( new ArrayList<>() ).isEmpty() );
+    }
+
+    @Test
+    public void filterDevices_compatible_unchanged() throws IOException {
+        final Management mgm = new Management();
+        DeviceInfo d = new DeviceInfo();
+        d.setName("abc");
+        d.setEnabled(true);
+        d.setClients(7);
+        final ArrayList<DeviceInfo> list = new ArrayList<>();
+        list.add( d );
+        final HashMap<String, Management.SurveyItem> map = mgm.filterDevices(list);
+        assertEquals("map size", map.size(), 1);
+        map.forEach( (key, survey) -> {
+            assertEquals( "name", d.getName(), key );
+            assertEquals( "clients", d.getClients(), survey.getClients() );
+            assertEquals( "isEnabled", d.isEnabled(), survey.isEnabled() );
+            assertEquals( "id -1", survey.getId(), -1 );
+        } );
+    }
+
+    @Test
+    public void filterDevices_doubleNames_singleName() throws IOException {
+        Management mgm = new Management();
+        DeviceInfo d1 = new DeviceInfo();
+        d1.setName("x" + TestUnicode.BAT_STRING + "d");
+        d1.setClients(7);
+        d1.setEnabled(false);
+        DeviceInfo d2 = new DeviceInfo();
+        d2.setName( d1.getName() );
+        d2.setClients(9);
+        d2.setEnabled(true);
+        final ArrayList<DeviceInfo> list = new ArrayList<>();
+        list.add(d1);
+        list.add(d2);
+        final HashMap<String, Management.SurveyItem> map = mgm.filterDevices(list);
+        assertEquals("map size", map.size(), 1);
+        Management.SurveyItem si = map.get( "x??d" );
+        assertNotNull( si );
+        assertEquals( "isEnabled", si.isEnabled(), d1.isEnabled() );
+        assertEquals( "clients", si.getClients(), d1.getClients() );
+        assertEquals( "id -1", si.getId(), -1 );
+        assertNotEquals( "isEnabled", si.isEnabled(), d2.isEnabled() );
+        assertNotEquals( "clients", si.getClients(), d2.getClients() );
     }
 }

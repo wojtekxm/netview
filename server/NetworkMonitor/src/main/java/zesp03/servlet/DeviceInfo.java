22e34304c0d8da0de143b984ac4033e49beaeaf0
Kacper Nitubydz <nitubydz@wp.pl>
2017-01-11 15:22:28 +0100

    front-end update 11.01.2017

--- server/NetworkMonitor/src/main/java/zesp03/servlet/DeviceInfo.java
+++ server/NetworkMonitor/src/main/java/zesp03/servlet/DeviceInfo.java
@@ -1,11 +1,15 @@
 package zesp03.servlet;
 
 
+import zesp03.core.App;
+import zesp03.data.CheckInfo;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -22,34 +26,60 @@ public class DeviceInfo extends HttpServlet {
 
 //        String green="greenDiode";
 //        String red="redDiode";
-        List<String> devices = new ArrayList<>();
+//        List<String> devices = new ArrayList<>();
+//        List<Integer> count = new ArrayList<>();
+//        String diodes[] = {"greenDiode", "redDiode"};
+//        for (int i = 0; i < 400; i++) {
+//            int idx = new Random().nextInt(diodes.length);
+//            int c = new Random().nextInt(50);
+//            String randDiode = (diodes[idx]);
+////            System.out.println(randDiode);
+//            devices.add(randDiode);
+//            if(randDiode.equals(diodes[0])){
+//                count.add(c);
+//                System.out.println(c);
+//            }else if(randDiode.equals(diodes[1])){
+//                count.add(0);
+//                System.out.println("0");
+//            }
+//        }
+//        System.out.println(devices.size());
+
+        List<String> diode = new ArrayList<>();
         List<Integer> count = new ArrayList<>();
-        String diodes[] = {"greenDiode", "redDiode"};
-        for (int i = 0; i < 400; i++) {
-            int idx = new Random().nextInt(diodes.length);
-            int c = new Random().nextInt(50);
-            String randDiode = (diodes[idx]);
-//            System.out.println(randDiode);
-            devices.add(randDiode);
-            if(randDiode.equals(diodes[0])){
-                count.add(c);
-                System.out.println(c);
-            }else if(randDiode.equals(diodes[1])){
-                count.add(0);
-                System.out.println("0");
-            }
+        ArrayList<CheckInfo> allDevices = null;
+        try {
+            allDevices = App.checkDevices();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        System.out.println(devices.size());
-        req.setAttribute("devicesList", devices);
+
+        for(CheckInfo dev : allDevices){
+
+            int number = dev.survey().getClientsSum();
+            count.add(number);
+
+            if(dev.survey().isEnabled()==true && dev.survey().getClientsSum()==0) {
+                diode.add("redDiode");
+            }else if(dev.survey().isEnabled()==true && dev.survey().getClientsSum()!=0){
+                diode.add("greenDiode");
+            }else if(dev.survey().isEnabled()==false){
+                diode.add("greyDiode");
+            }
+
+            dev.device().getId();
+            dev.device().getControllerId();
+            dev.device().getDescription();
+        }
+        req.setAttribute("devicesList", diode);
         req.setAttribute("countList", count);
         req.getRequestDispatcher("logged.jsp").forward(req,resp);
     }
 
-        private static final Random RANDOM = new Random();
-
-        public int getNextInt() {
-            return RANDOM.nextInt();
-        }
+//        private static final Random RANDOM = new Random();
+//        public int getNextInt() {
+//            return RANDOM.nextInt();
+//        }
 
 }
 

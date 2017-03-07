85062e01c4bddc67b7845a53c45c6334039514d1
faku <faku@mat.umk.pl>
2017-03-07 14:14:00 +0100

    api wyświetla wszystkie budynki, jednostki, budynki jednostki http://localhost:8081/api/all-units http://localhost:8081/api/all-buildings http://localhost:8081/api/all-link-units-buildings

---
+++ server/webapp/src/main/java/zesp03/rest/resource/LinkUnitBuildingResource.java
@@ -0,0 +1,46 @@
+package zesp03.rest.resource;
+
+import zesp03.common.Database;
+import zesp03.data.row.LinkUnitBuildingRow;
+import zesp03.entity.Link_unit_building;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.ws.rs.*;
+
+/**
+ * Created by Berent on 2017-03-06.
+ */
+
+@Path("link-unit-building")
+public class LinkUnitBuildingResource {
+    @GET
+    @Produces("application/json")
+    public LinkUnitBuildingRow getLinkUnitBuilding(
+            @QueryParam("id") long id) {
+        LinkUnitBuildingRow result = null;
+
+        EntityManager em = null;
+        EntityTransaction tran = null;
+        try {
+            em = Database.createEntityManager();
+            tran = em.getTransaction();
+            tran.begin();
+
+            Link_unit_building lub = em.find(Link_unit_building.class, id);
+            if (lub != null)
+                result = new LinkUnitBuildingRow(lub);
+
+            tran.commit();
+        } catch (RuntimeException exc) {
+            if (tran != null && tran.isActive()) tran.rollback();
+            throw exc;
+        } finally {
+            if (em != null) em.close();
+        }
+
+        if (result == null)
+            throw new NotFoundException();
+        return result;
+    }
+}

9d3e501ea17db3b229a093b29deb43caf2d61381
Wojtek Marciniak <wojtekxm@gmail.com>
2017-03-19 23:07:42 +0100

    generator badań tworzy mniej chaotyczne wyniki

--- server/common/src/main/java/zesp03/common/service/FakeNetworkServiceImpl.java
+++ server/common/src/main/java/zesp03/common/service/FakeNetworkServiceImpl.java
@@ -20,10 +20,16 @@ import java.util.Random;
  */
 @Service
 public class FakeNetworkServiceImpl implements NetworkService {
+    private static class VirtualDevice {
+        String name;
+        int lastClients;
+        boolean lastEnabled;
+    }
     /**
-     * mapuje adres IP kontrolera do listy nazw urządzeń które są zarządzane przez ten kontroler
+     * mapuje adres IP kontrolera do listy urządzeń które są zarządzane przez ten kontroler
      */
-    private final HashMap<String, ArrayList<String>> map = new HashMap<>();
+    private final HashMap<String, ArrayList<VirtualDevice>> map = new HashMap<>();
+    private final Random random;
 
     /**
      * wczytuje dane z plików konfiguracyjnych FakeSNMP_***.txt
@@ -33,6 +39,7 @@ public class FakeNetworkServiceImpl implements NetworkService {
      * @throws IOException błąd przy czytaniu plików konfiguracyjnych
      */
     public FakeNetworkServiceImpl() throws IOException {
+        random = new Random();
         parse("FakeNetworkServiceImpl_wifi02.txt");
         parse("FakeNetworkServiceImpl_wifi03.txt");
         parse("FakeNetworkServiceImpl_wifi05.txt");
@@ -57,14 +64,19 @@ public class FakeNetworkServiceImpl implements NetworkService {
                 if (controllerIP == null)
                     throw new IllegalStateException("invalid resource for FakeNetworkServiceImpl, IP is missing");
                 controllerIP = controllerIP.trim().intern();
-                final ArrayList<String> names = new ArrayList<>();
+                final ArrayList<VirtualDevice> devices = new ArrayList<>();
                 String line;
                 while ((line = reader.readLine()) != null) {
                     line = line.trim().intern();
-                    if (line.length() > 0)
-                        names.add(line);
+                    if (line.length() > 0) {
+                        VirtualDevice vd = new VirtualDevice();
+                        vd.name = line;
+                        vd.lastClients = random.nextInt(50);
+                        vd.lastEnabled = choose(0.8);
+                        devices.add(vd);
+                    }
                 }
-                map.put(controllerIP, names);
+                map.put(controllerIP, devices);
             }
         } finally {
             if (stream != null) stream.close();
@@ -75,18 +87,46 @@ public class FakeNetworkServiceImpl implements NetworkService {
     public List<SurveyInfo> queryDevices(String controllerIP) throws SNMPException {
         if (!map.containsKey(controllerIP))
             throw new SNMPException("unable to connect with specified controller");
-        final ArrayList<String> names = map.get(controllerIP);
+        final ArrayList<VirtualDevice> devices = map.get(controllerIP);
         final Random random = new Random();
         final ArrayList<SurveyInfo> result = new ArrayList<>();
-        for (String name : names) {
-            SurveyInfo ds = new SurveyInfo();
-            ds.setName(name);
-            // 80% szansy że będzie włączony
-            ds.setEnabled(random.nextInt(5) < 4);
-            // losowa liczba klientów z przedziału 0 .. 49
-            ds.setClientsSum(random.nextInt(50));
-            result.add(ds);
+        for (VirtualDevice vd : devices) {
+            SurveyInfo si = new SurveyInfo();
+            si.setName(vd.name);
+            boolean enabled;
+            int clients;
+            int diff;
+            if(choose(0.9)) {
+                diff = random.nextInt(9) - 4;
+            }
+            else {
+                diff = random.nextInt(41) - 20;
+            }
+            if(vd.lastEnabled) {
+                enabled = choose(0.99);
+            }
+            else {
+                enabled = choose(0.2);
+            }
+            if(enabled) {
+                clients = vd.lastClients + diff;
+                if(clients < 0)clients = 0;
+                if(clients > 70)clients = 65;
+            }
+            else {
+                clients = 0;
+            }
+            si.setEnabled(enabled);
+            si.setClientsSum(clients);
+            vd.lastEnabled = enabled;
+            vd.lastClients = clients;
+            result.add(si);
         }
         return result;
+    }
+
+    private boolean choose(double chance) {
+        int limit = (int)Math.round(chance * 1000_000_000);
+        return random.nextInt(1000_000_000) < limit;
     }
 }
